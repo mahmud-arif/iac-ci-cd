5f94e769a54890385236d6b479d20512ad34ea02
arif mahmud <mahmudarif1175@gmail.com>
2024-10-03 23:58:10 +0600

    remove all infra except vpc

--- Blueprint/main.tf
+++ Blueprint/main.tf
@@ -3,23 +3,23 @@ provider "aws" {
   alias  = "virginia"
 }
 
-data "aws_eks_cluster_auth" "this" {
-  name = module.eks_cluster.cluster_name
-}
-provider "kubernetes" {
-  host                   = module.eks_cluster.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
+# data "aws_eks_cluster_auth" "this" {
+#   name = module.eks_cluster.cluster_name
+# }
+# provider "kubernetes" {
+#   host                   = module.eks_cluster.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
+#   token                  = data.aws_eks_cluster_auth.this.token
+# }
 
-provider "helm" {
-  kubernetes {
-    host                   = module.eks_cluster.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
+# provider "helm" {
+#   kubernetes {
+#     host                   = module.eks_cluster.cluster_endpoint
+#     cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
+#     token                  = data.aws_eks_cluster_auth.this.token
+#   }
 
-}
+# }
 
 
 data "aws_caller_identity" "current" {}
