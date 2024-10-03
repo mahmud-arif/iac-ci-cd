211eb4083fcc75153b0a87f9163ca4b3293d5bd1
arif mahmud <mahmudarif1175@gmail.com>
2024-10-03 23:53:00 +0600

    remove all infra

--- Blueprint/main.tf
+++ Blueprint/main.tf
@@ -28,57 +28,57 @@ data "aws_ecrpublic_authorization_token" "token" {
   provider = aws.virginia
 }
 
-module "myapp_vpc" {
-  source          = "../modules/vpc"
-  name            = "${var.environment}-${var.vpc_name}"
-  cidr            = var.vpc_cidr_block
-  private_subnets = var.private_subnet_cidr_blocks
-  public_subnets  = var.public_subnet_cidr_blocks
-  azs             = var.vpc_availability_zones
+# module "myapp_vpc" {
+#   source          = "../modules/vpc"
+#   name            = "${var.environment}-${var.vpc_name}"
+#   cidr            = var.vpc_cidr_block
+#   private_subnets = var.private_subnet_cidr_blocks
+#   public_subnets  = var.public_subnet_cidr_blocks
+#   azs             = var.vpc_availability_zones
 
-  enable_nat_gateway     = var.vpc_enable_nat_gateway
-  single_nat_gateway     = var.vpc_single_nat_gateway
-  enable_dns_hostnames   = var.vpc_dns_hostname
-  one_nat_gateway_per_az = var.one_nat_gateway_per_az
-  enable_dns_support     = var.enable_dns_support
-  tags                   = var.common_tags
-  public_subnet_tags     = var.public_subnet_tags
-  private_subnet_tags    = var.private_subnet_tags
-}
-
-
-
-module "eks_cluster" {
-  source = "../modules/eks"
-
-  cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
-  subnet_ids      = module.myapp_vpc.private_subnets
-  vpc_id          = module.myapp_vpc.vpc_id
+#   enable_nat_gateway     = var.vpc_enable_nat_gateway
+#   single_nat_gateway     = var.vpc_single_nat_gateway
+#   enable_dns_hostnames   = var.vpc_dns_hostname
+#   one_nat_gateway_per_az = var.one_nat_gateway_per_az
+#   enable_dns_support     = var.enable_dns_support
+#   tags                   = var.common_tags
+#   public_subnet_tags     = var.public_subnet_tags
+#   private_subnet_tags    = var.private_subnet_tags
+# }
 
 
-  enable_cluster_creator_admin_permissions = true
-  cluster_endpoint_public_access           = var.cluster_endpoint_public_access
+
+# module "eks_cluster" {
+#   source = "../modules/eks"
+
+#   cluster_name    = var.cluster_name
+#   cluster_version = var.cluster_version
+#   subnet_ids      = module.myapp_vpc.private_subnets
+#   vpc_id          = module.myapp_vpc.vpc_id
+
+
+#   enable_cluster_creator_admin_permissions = true
+#   cluster_endpoint_public_access           = var.cluster_endpoint_public_access
   
 
 
-  eks_managed_node_groups = var.eks_managed_node_groups
+#   eks_managed_node_groups = var.eks_managed_node_groups
 
-}
+# }
 
-module "eks_auth" {
-  source = "aidanmelen/eks-auth/aws"
-  eks    = module.eks_cluster
-### user
-  map_roles = [
-    {
-      rolearn  = "arn:aws:iam::058264357476:role/AdministratorAccess-Role"
-      username = "devopsadmin"
-      groups   = ["system:masters"]
-    }
-  ]
+# module "eks_auth" {
+#   source = "aidanmelen/eks-auth/aws"
+#   eks    = module.eks_cluster
+# ### user
+#   map_roles = [
+#     {
+#       rolearn  = "arn:aws:iam::058264357476:role/AdministratorAccess-Role"
+#       username = "devopsadmin"
+#       groups   = ["system:masters"]
+#     }
+#   ]
 
-}
+# }
 
 
 
